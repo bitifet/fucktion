b1b87c0394f819ce01af72d0b96caf351c011bc6
Christopher Brown <chbphone55@gmail.com>
2019-03-20 02:04:27 -0600

    better readabililty and documentation

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
-#!/usr/bin/env node
-"use strict";
-var Promise = require("bluebird");
-var Deasync = require("deasync");
+const deasync = require('deasync');
+const fucktion = {};
 
-// The ugly thing:
-function fucktion_promise(fn){
-    return function(){
-        var that = this;
-        return Promise.all(
-            [].slice.call(arguments)
-                //.map(x=>Promise.resolve(x))
-                    // Works with native promises.
-                .map(Promise.resolve)
-                    // Throws "TypeError: function resolve() { [native code] } called on non-object"
-                    // with native promises, but works perfectly with BlueBird.
-        ).then(function(args){
-            return fn.apply(that, args);
-        });
-        ;
-
-    };
+/**
+ * Creates an async function wrapper that awaits all arguments
+ * @param {function} func
+ */
+fucktion.promise = func => {
+  return async (...args) => {
+    return func(...(await Promise.all(args)));
+  };
 };
 
-// The uglier thing:
-function fucktion_cbk(fn){
-    var pfn = fucktion_promise(fn);
-    return function(){
-        var args = [].slice.call(arguments);
-        var cbk = args.pop();
-        pfn.apply(this, args)
-            .then(function(result){
-                cbk(null, result);
-            }).catch(function(err){
-                cbk(err);
-            })
-        ;
-    };
+/**
+ * @callback fucktionCallback
+ * @param {Error | null} error
+ * @param {any} result
+ */
+/**
+ * A wrapper for `fucktion.promise` that uses a callback you
+ * provide as the last argument to your function. The signature
+ * for the callback is `(error: Error | null, result: any) => void`
+ *
+ * @param {function} func
+ */
+fucktion.callback = func => {
+  const promisifiedFunction = fucktion.promise(fn);
+  return (...args) => {
+    /** @type fucktionCallback */
+    const callback = args.pop();
+    promisifiedFunction(args)
+      .then(result => callback(null, result))
+      .catch(err => callback(err));
+  };
 };
 
-// The ugliest thing:
-function fucktion_sync(fn){
-    return Deasync(fucktion_cbk(fn));
-};
+/**
+ * Wraps `fucktion.promise` with `deasync` allowing you to pass
+ * promises to your function but still use it synchronously.
+ * @param {function} func
+ */
+fucktion.sync = func => deasync(fucktion.promise(func));
 
-module.exports = {
-    promise: fucktion_promise,
-    cbk: fucktion_cbk,
-    sync: fucktion_sync,
-};
-
-
+module.exports = fucktion;
